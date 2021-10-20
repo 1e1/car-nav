9601383d18a8f1df01116a6a5d47f33f3ecd0682
1e1 <2.71828183e0+git@gmail.com>
2021-10-20 18:19:28 +0200

    refactoring monofile to html/css/js files

---
+++ assets/script/module/MovingData.js
@@ -0,0 +1,127 @@
+class MovingData {
+    constructor() {
+        this.vector = {
+            heading: 321,
+            speed: 0,
+            timestamp: Date.now(),
+        };
+        this.dvector = {
+            heading: 0,
+            speed: 0,
+            timestamp: 1,
+        },
+        this.position = {
+            longitude: -1.658711,
+            latitude: 48.117881,
+            altitude: 1,
+            timestamp: Date.now(),
+        };
+        this.destination = null;
+        this.trip = null;
+    }
+
+    setVector(heading, speed) {
+        const now = Date.now();
+        const dtime = now - this.vector.timestamp;
+
+        this.dvector = {
+            heading: (heading-this.vector.heading) * 1000/dtime,
+            speed: (speed-this.vector.speed) * 1000/dtime, 
+            timestamp: dtime,
+        };
+
+        this.vector.heading = 0 + heading;
+        this.vector.speed = 0 + speed;
+        this.vector.timestamp = now;
+    }
+
+    setPosition(longitude, latitude, altitude) {
+        this.position.longitude = longitude;
+        this.position.latitude  = latitude;
+        this.position.altitude  = altitude;
+        this.position.timestamp = Date.now();
+    }
+
+    getPosition2dAsArray() {
+        return [ this.position.longitude, this.position.latitude ];
+    }
+
+    getPosition3dAsArray() {
+        return this.getPosition2dAsArray().concat([this.position.altitude]);
+    }
+
+    getVirtualClone() {
+        if (this.dvector.timestamp == 1) {
+            return null;
+        }
+
+        const earthRadiusInM = 6372795.477598;
+        const deg2rad = deg => Math.PI * deg / 180;
+        const rad2deg = rad => 180 * rad / Math.PI;
+        const now = Date.now();
+        const dtime = now - this.position.timestamp;
+        const speed = this.vector.speed + (this.dvector.speed*dtime/1000);
+        const distanceInM = speed * this.dvector.timestamp /1000;
+        const heading = this.vector.heading + (this.dvector.heading*dtime/1000);
+        const headingInRad = deg2rad(heading);
+        const ratio = distanceInM / earthRadiusInM;
+        const lngInRad = deg2rad(this.position.longitude);
+        const latInRad = deg2rad(this.position.latitude);
+
+        const nextLatInRad = Math.asin( 
+            (Math.sin(latInRad)*Math.cos(ratio)) +
+            (Math.cos(latInRad)*Math.sin(ratio)*Math.cos(headingInRad))
+        );
+        
+        const nextLngInRad = lngInRad + 
+            Math.atan2(
+                Math.sin(headingInRad)*Math.sin(ratio)*Math.cos(latInRad),
+                Math.cos(ratio) - (Math.sin(latInRad)*Math.sin(nextLatInRad))
+            )
+        ;
+
+        return {
+            vector: {
+                heading: heading,
+                speed: speed,
+                timestamp: now,
+            },
+            position: {
+                longitude: rad2deg(nextLngInRad),
+                latitude:  rad2deg(nextLatInRad),
+                timestamp: now,
+            }
+        }
+    }
+
+    resetTrip() {
+        this.destination = null;
+        this.trip = null;
+    }
+
+    setTripDestination(longitude, latitude) {
+        this.destination = {
+            longitude: longitude,
+            latitude:  latitude,
+            timestamp: Date.now(),
+        };
+    }
+
+    getTripDestination2dAsArray() {
+        return [ this.destination.longitude, this.destination.latitude ];
+    }
+
+    setTripInfo(durationInMs, distanceInM) {
+        this.trip = {
+            duration: durationInMs,
+            distance: distanceInM,
+            timestamp: Date.now(),
+        };
+    }
+
+    getTripEtaInDate() {
+        return (this.trip != null) ? new Date(this.trip.durartion + this.trip.timestamp) : Date.now();
+    }
+}
+
+export { MovingData };
---
+++ assets/script/module/Tracer.js
@@ -0,0 +1,99 @@
+class Tracer {
+    constructor(callback) {
+        this.callback = callback;
+        this.pid = null;
+        this.rawPosition = null;
+        this.smoothPosition = null;
+
+        this.decay = 1.2;
+        this.variance = -1;
+        this.altitudeVariance = -1;
+        this.minAccuracy = 1;
+    }
+
+    update(position) {
+        this.rawPosition = position;
+
+        const localTimestamp = Date.now();
+
+        let accuracy = 0+ position.coords.accuracy;
+        let altitudeAccuracy = 0+ position.coords.altitudeAccuracy;
+
+        if (accuracy < this.minAccuracy) {
+            accuracy = this.minAccuracy;
+        }
+
+        if (altitudeAccuracy < this.minAccuracy) {
+            altitudeAccuracy = this.minAccuracy;
+        }
+
+        if (this.variance < 0) {
+            this.smoothPosition = {
+                coords: {
+                    accuracy: position.coords.accuracy,
+                    longitude: position.coords.longitude,
+                    latitude: position.coords.latitude,
+                    altitudeAccuracy: position.coords.altitudeAccuracy,
+                    altitude: position.coords.altitude,
+                    heading: position.coords.heading,
+                    speed: position.coords.speed,
+                },
+                timestamp: position.timestamp,
+                localTimestamp: localTimestamp,
+            };
+            this.variance = accuracy * accuracy;
+            this.altitudeVariance = altitudeAccuracy * altitudeAccuracy;
+        } else {
+            const timeIncMs = position.timestamp - this.smoothPosition.timestamp;
+            const coeffVariance = (timeIncMs * this.decay * this.decay) / 1000;
+
+            if (timeIncMs > 0) {
+                this.variance += coeffVariance;
+                this.altitudeVariance += coeffVariance;
+                this.smoothPosition.timestamp = position.timestamp;
+            }
+
+            const deltaVariance = this.variance / (this.variance + (accuracy*accuracy));
+            this.smoothPosition.coords.longitude += deltaVariance * (position.coords.longitude - this.smoothPosition.coords.longitude);
+            this.smoothPosition.coords.latitude += deltaVariance * (position.coords.latitude - this.smoothPosition.coords.latitude);
+            this.variance = (1 - deltaVariance) * this.variance;
+
+            const deltaAltitudeVariance = this.altitudeVariance / (this.altitudeVariance + (altitudeAccuracy*altitudeAccuracy));
+            this.smoothPosition.coords.altitude += deltaAltitudeVariance * (position.coords.altitude - this.smoothPosition.coords.altitude);
+            this.altitudeVariance = (1 - deltaAltitudeVariance) * this.altitudeVariance;
+
+            this.smoothPosition.localTimestamp = localTimestamp;
+        }
+    }
+
+    getRawPosition() {
+        return this.rawPosition;
+    }
+
+    getSmoothPosition() {
+        return this.smoothPosition;
+    }
+
+    start() {
+        if (this.pid == null) {
+            this.pid = navigator.geolocation.watchPosition(position => {
+                this.update(position);
+                this.callback(this);
+            },
+                error => {
+                // console.debug(error); // TODO
+            },
+            {
+                enableHighAccuracy: true,
+                timeout: 4321,
+                maximumAge: 666,
+            });
+        }
+    }
+
+    stop() {
+        navigator.geolocation.clearWatch(this.pid);
+    }
+}
+
+export { Tracer };
